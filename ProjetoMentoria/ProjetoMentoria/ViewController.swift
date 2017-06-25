30b522fd69cab3946b596cd7c5ff143d57408e5f
Thiago Moura <thiago.moura@youse.com.br>
2017-06-25 10:25:16 -0300

    Initial Structure

---
+++ ProjetoMentoria/ProjetoMentoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetoMentoria
+//
+//  Created by Thiago Moura on 25/06/17.
+//  Copyright © 2017 Thiago Moura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
