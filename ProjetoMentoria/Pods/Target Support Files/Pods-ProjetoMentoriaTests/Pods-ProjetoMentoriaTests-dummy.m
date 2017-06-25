faf5611abb001ca6c726f3542af829d6157a9432
Thiago Moura <thiago.moura@youse.com.br>
2017-06-25 10:38:12 -0300

    Add and install pod on project

---
+++ ProjetoMentoria/Pods/Target Support Files/Pods-ProjetoMentoriaTests/Pods-ProjetoMentoriaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjetoMentoriaTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjetoMentoriaTests
+@end
---
+++ ProjetoMentoria/Pods/Target Support Files/Pods-ProjetoMentoriaUITests/Pods-ProjetoMentoriaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjetoMentoriaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProjetoMentoriaUITests
+@end
